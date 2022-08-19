a1e12040f87b5163703e2257d4087194de3f2843
Finger <tangqianjin@healbone.xyz>
2022-08-19 15:32:07 +0800

    feat: 空间点位计算分解各个方向上的加速度

--- acceleration.py
+++ acceleration.py
@@ -1,14 +1,14 @@
 from typing import Tuple, List
 
 
-def calculateVelocity(point1: tuple, point2: tuple, time: float)->List[float]:
+def calculateVelocity(point1: tuple, point2: tuple, time: float) -> Tuple[float]:
     velocity_x = (point2[0] - point1[0]) / time
     velocity_y = (point2[1] - point1[1]) / time
     velocity_z = (point2[2] - point1[2]) / time
     return velocity_x, velocity_y, velocity_z
 
 
-def calculateAcceleration(point1: tuple, point2: tuple, time: float)->List[float]:
+def calculateAcceleration(point1: tuple, point2: tuple, time: float) -> List[float]:
     acceleration = [velocity / time for velocity in calculateVelocity(point1, point2, time)]
     return acceleration
 
